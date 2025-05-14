dd1d660b805c46b2f5238f840c02d5b1487dce57
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2025-05-14 19:41:39 +0000

    first commit

---
+++ Lista.java
@@ -0,0 +1,19 @@
+public class Lista<T> {
+
+        private Nodo<T> root;
+
+            public Lista() {
+                    root = null;
+                        }
+
+                            public void add(T newval) {
+                                    if (root==null) {
+                                                root = new Nodo<>(newval);
+                                                        } else {
+                                                                    Nodo<T> temp = root;
+                                                                                while (temp.getSucc()!=null) temp=temp.getSucc();
+                                                                                            temp.setSucc(new Nodo<>(newval));
+                                                                                                    }
+                                                                                                        }
+                                                                                                        }
+}
