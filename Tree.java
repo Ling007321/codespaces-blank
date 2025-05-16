5ca4cc8188926869107e84321baa4096decdd1c4
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2025-05-16 08:28:07 +0000

    7

--- Tree.java
+++ Tree.java
@@ -1,7 +1,17 @@
-public class Tree<T> {
+public class Tree<T extends Comparable<T>> {
     protected Treenode<T> root;
     
     public Tree() {
         root = null;
     }
+
+    public void add(T value) {
+        Treenode<T> newNode = new Treenode<>(value);
+        if (root == null) {
+            root = newNode;
+        } else {
+            add(value);
+        }
+    }
+
 }
